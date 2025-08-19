af644a16248ab356e32574bd9ac6eb4937672553
Sparsh <sparsh.raj30@gmail.com>
2025-08-19 13:46:08 +0530

    Revert "Merge pull request #1 from spa-raj/data-ingestion"
    This reverts commit b554665f65cc7bab5d9b113791c2f5208a9b29b2, reversing
    changes made to 778d78cb083d76e036306224aafa35fe99af2d10.

--- infra/terraform/s3/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "bucket_name"       { value = aws_s3_bucket.slack.bucket }
-output "bucket_arn"        { value = aws_s3_bucket.slack.arn }
-output "writer_policy_arn" { value = try(aws_iam_policy.writer[0].arn, null) }
